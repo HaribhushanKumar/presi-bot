67ef845af47aa9521435de66ffca8f4a898c467a
HaribhushanKumar <ranjank0680039@gmail.com>
2025-12-24 22:41:55 +0530

    Initial project commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="presi_bot",
+    version="0.1.0",
+    author="Ranjan Kumar",
+    author_email="rpy6425@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
